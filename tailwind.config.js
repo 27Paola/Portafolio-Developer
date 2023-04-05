191424288fe79dda2e619d2c978a94474a817ea2
Paola Sanchez <yineth2paola2@gmail.com>
2023-04-05 02:51:44 -0500

    create of the Home

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: "class",
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+      },
+      colors: {
+        'dark': '#303234',
+        'yellow': '#eccba3',
+        'pink': '#f48b8b',
+        'blue': '#38baa6',
+      },
+      fontFamily: {
+        Caveat: ['"Caveat", "cursive"'],
+        DynaPuff: ['"DynaPuff", "cursive"'],
+        Gochi: ['"Gochi Hand", "cursive"'],
+        Quicksand: ['"Quicksand", "sans-serif"']
+      },
+    },
   },
   plugins: [],
 }
