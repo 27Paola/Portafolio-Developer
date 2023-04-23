fd0781ea542ad386b2345bbdac2b57aba031d990
Paola Sanchez <yineth2paola2@gmail.com>
2023-04-23 16:44:03 -0500

    corrections of button

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,37 +1,19 @@
 import React from 'react';
-import { useContext } from 'react';
-import { GiSunCloud, GiMoonOrbit } from 'react-icons/gi';
+import { CgMoon } from 'react-icons/cg';
+import { BiSun } from 'react-icons/bi'
 import Logo from '../assets/logo  paola.png';
 import LogoWhite from '../assets/image (1).png';
-import { FiGithub, FiLinkedin } from 'react-icons/fi'
-import { AiOutlineWhatsApp } from 'react-icons/ai'
+import { TbBrandWhatsapp} from 'react-icons/tb'
 import HamburguerMenu from './HamburguerMenu';
+import { TiSocialGithubCircular, TiSocialLinkedinCircular } from 'react-icons/ti';
 
 const Navbar = ({ darkMode, setDarkMode }) => {
-    {/* // ----REDES SOCIALES----- */ }
-    const REDES = [
-        {
-            id: 1,
-            Link: "https://github.com/27Paola",
-            icon: <FiGithub />
-        },
-        {
-            id: 2,
-            Link: "https://www.linkedin.com/in/paola-sanchez-/",
-            icon: <FiLinkedin />
-        },
-        {
-            id: 3,
-            Link: "https://wa.link/fih42v",
-            icon: <AiOutlineWhatsApp />
-        }
-    ]
     return (
         <div className='bg-white dark:bg-dark text-blue dark:text-pink fixed z-40 w-full shadow-sm shadow-black/20 '>
             <nav className='flex justify-between items-center p-5'>
 
                 {/* ------Logo------- */}
-                <div onClick={() => setDarkMode(!darkMode)}>
+                <div setDarkMode={() => setDarkMode(!darkMode)}>
                     {
                         darkMode ?
                             <img src={LogoWhite} width={60} />
@@ -39,23 +21,30 @@ const Navbar = ({ darkMode, setDarkMode }) => {
                             <img src={Logo} width={50} />
                     }
                 </div>
+
                 {/* -----REDES SOCIALES--- */}
-                <div className='flex justify-center items-center gap-4 text-2xl w-full md:gap-6 md:text-3xl'>
-                    {REDES.map(({ id, Link, icon }) => (
-                        <a href={Link}
-                            key={id} target='_blank'
-                            className='curson-pointer duration-300 hover:text-pink'
-                        >{icon}</a>
-                    ))}
+
+                <div className='flex justify-center items-center gap-4 pt-3 text-2xl w-full md:gap-4 md:text-3xl'>
+                    <a href="https://github.com/27Paola" target='_black' className=" cursor-pointer text-4xl lg:text-6xl rounded-full hover:scale-110"><TiSocialGithubCircular /></a>
+                    <a href="https://www.linkedin.com/in/paola-sanchez-/" target='_black' className="text-4xl lg:text-6xl rounded-full hover:scale-110" ><TiSocialLinkedinCircular /></a>
+                    <a href="https://wa.link/fih42v" target='_black' className=" cursor-pointer text-3xl lg:text-5xl rounded-full hover:scale-110"><TbBrandWhatsapp /></a>
                 </div>
 
+                {/* <div className='flex justify-center items-center gap-4 text-2xl w-full md:gap-6 md:text-3xl'>
+                        <a href="https://github.com/27Paola" className='curson-pointer rounded-full border-2 hover:scale-150 duration-300 hover:text-pink p-3'><GoMarkGithub /></a>
+
+                        <a href="https://www.linkedin.com/in/paola-sanchez-/" className='curson-pointer rounded-full border-2 hover:scale-150 duration-300 hover:text-pink p-3'><FiLinkedin /></a>
+
+                        <a href="https://wa.link/fih42v" className='curson-pointer rounded-full border-2 hover:scale-150 duration-300 hover:text-pink p-3'><AiOutlineWhatsApp /></a> 
+                </div> */}
+
                 {/*----Iconos-----  */}
-                <div className='fixed bottom-6 right-6 z-50 text-[#1c130e] dark:text-white bg-pink p-3 rounded-full animate-bounce' onClick={() => setDarkMode(!darkMode)}>
+                <div className='fixed bottom-6 right-6 z-50 text-white dark:text-dark bg-pink p-3 rounded-full border-2 border-white dark:border-dark animate-bounce' onClick={() => setDarkMode(!darkMode)}>
                     {
                         darkMode ?
-                            <GiMoonOrbit className='text-4xl cursor-pointer' />
+                            <BiSun className='text-3xl md:text-4xl cursor-pointer' />
                             :
-                            <GiSunCloud className='text-4xl cursor-pointer' />
+                            <CgMoon className='text-3xl md:text-4xl cursor-pointer' />
                     }
                 </div>
                 <HamburguerMenu />
